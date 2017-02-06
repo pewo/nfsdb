8b688e72441436ab19239147acd32436eae1302d
Peter Wirdemo <peter.wirdemo@gmail.com>
2017-02-06 13:41:11 +0100

    A lot of changes

--- nfsdb.pl
+++ nfsdb.pl
@@ -11,20 +11,21 @@ use JSON; # imports encode_json, decode_json, to_json and from_json.
 
 my($host) = hostname;
 my($localtime) = scalar localtime(time);
-my($maxpath) = 5;
+my($maxdepth) = 2;
 sub maxpath($) {
 	my($path) = shift;
 	my(@arr) = split(/\//,$path);
-	if ( $#arr >= $maxpath ) {
+	my($depth) = $#arr;
+	#print "path=[$path], depth=[$depth]\n";
+	if ( $#arr >= $maxdepth ) {
 		$path = "";
-		my($i) = $maxpath;
+		my($i) = $maxdepth;
 		while ( $i-- > 0 ) {
 			$path .= "/" . shift(@arr);
 		}
-		#$path = join("/",$arr[0],$arr[1],$arr[2], $arr[3]);
 		$path =~ s/^\/\//\//;
 	}
-	return($path);
+	return($path,$depth);
 }
 
 
@@ -33,7 +34,6 @@ $comment .= "#\n";
 $comment .= "# Created by $0 on $host\n";
 $comment .= "# Timestamp $localtime\n";
 $comment .= "#\n";
-
 
 # Set the variable $File::Find::dont_use_nlink if you're using AFS,
 # since AFS cheats.
@@ -50,27 +50,52 @@ my(%res);
 sub wanted {
 
 	my($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,$atime,$mtime,$ctime,$blksize,$blocks) = lstat($_);
+	# Do not enter .snapshot
 	if ( /^\.snapshot\z/s ) {
 		$File::Find::prune = 1;
 		return;
 	}
 
+	# Keep us inside starting filesystem
+	if ( $dev != $File::Find::topdev ) {
+		$File::Find::prune = 1;
+		return;
+	}
+
+	return unless ( -f $_ );
+
 	return unless ( $mtime );
-	#print "dir: " . $dir . " file: " . $name . "\n";
-	my($maxpath) = maxpath($dir);
+	my($maxpath,$depth) = maxpath($dir);
+	#print "dir: " . $dir . " file: " . $name . " maxpath: $maxpath, depth=$depth\n";
 	$res{$maxpath}{size}+=$size;
 	$res{$maxpath}{time}=time;
 	$res{$maxpath}{files}++;
 }
 
-my($i) = 0;
 my($subdir) = shift(@ARGV);
 if ( $subdir ) {
-	$i++ if ( -d $subdir );
+	#print "Subdir=[$subdir]\n";
+	$subdir = dirname($subdir) . "/" . basename($subdir);
+	#print "Subdir=[$subdir]\n";
 }
-die "Usage: $0 <directory>\n" unless ( $i ) ;
+die "Usage: $0 <directory>\n" unless ( -d $subdir ) ;
+
+# Calculate maxdepth based on depth on searchdir + $maxdepth (1)
+my($dummy,$depth) = maxpath($subdir);
+$maxdepth += $depth;
 
 print $comment;
+
+my(%meta) = (
+	HEADER => 1,
+	OUTPUT => "json",
+	VERSION => 1,
+	TYPE => "nfs",
+	DIR => $subdir,
+	EPOC => time,
+	MAXDEPTH => $maxdepth,
+);
+print encode_json(\%meta) . "\n";
 
 my($filename) = $subdir;
 $filename =~ s/\W/_/g;
@@ -78,9 +103,10 @@ my($runtime) = time - $BASETIME;
 sleep 1;
 File::Find::find({wanted => \&wanted}, $subdir);
 
-$i = 0;
+my($i) = 0;
 my(%sum);
 my($path);
+#print Dumper(\%res);
 foreach $path ( keys %res ) {
 	#print "Dir: $path\n";
 	my($dirpath) = "";
@@ -89,31 +115,34 @@ foreach $path ( keys %res ) {
 		$dirpath =~ s/^\/*/\//;
 		$sum{$dirpath}{size}+=$res{$path}{size};
 		$sum{$dirpath}{files}+=$res{$path}{files};
-		#$sum{$dirpath}{path}=$dirpath;
 		my($shortpath) = $dirpath;
 		$shortpath =~ s/^$subdir//;
 		$sum{$dirpath}{path}=$shortpath;
+		#$sum{$dirpath}{path}=$dirpath;
 		
 	}
 	#last if ( $i++ > 10 );
 }
 
-my($meta) = "";
-$meta .= "%OUPUT=JSON\n";
-$meta .= "%VERSION=1\n";
-$meta .= "%TYPE=NFS\n";
-$meta .= "%DIR=$subdir\n";
-print $meta;
+#print Dumper(\%sum);
 
 my($sumdir);
 foreach $sumdir ( keys %sum ) {
 	#print "sumdir=[$sumdir] subdir=$subdir\n";
-	next unless ( $sumdir =~ /^$subdir/ );
+	unless ( $sumdir =~ /^$subdir/ ) {
+		#print "Skipping [$sumdir] it is not inside [$subdir]\n";
+		next;
+	}
 	my($hp) = $sum{$sumdir};
-	my($json) = encode_json $hp;
+	my(@arr) = ();
+	push(@arr,"path",$hp->{path});
+	delete($hp->{path});
+	foreach ( sort keys %$hp ) {
+		#print "Key=$_, value=$hp->{$_}\n";
+		push(@arr,$_,$hp->{$_});
+	}
+	#my($json) = encode_json $hp;
+	my($json) = encode_json \@arr;
 	print "$json\n";
 }
 #print Dumper(\%sum);
-
-
-
